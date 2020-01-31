b6884729a31ae1840e8ba01bfc44586a9fe9102d
1802146 <tamakireaper@gmail.com>
2020-01-31 11:15:26 -0500

    Tableaux test final

---
+++ TableauxTest.java
@@ -0,0 +1,79 @@
+import static org.junit.Assert.*;
+
+public class TableauxTest {
+
+    @org.junit.Test
+    public void trierCroissantSSS() {
+        int[] tab = {5,4,2,3,1,6,7,9,8};
+        int[] expectedTab = {1,2,3,4,5,6,7,8,9};
+        Tableaux.trierCroissantSSS(tab);
+        assertArrayEquals(expectedTab, tab);
+
+    }
+
+    @org.junit.Test
+    public void trierDecroissantSSS() {
+        int[] tab = {5,4,2,3,1,6,7,9,8};
+        int[] expectedTab = {9,8,7,6,5,4,3,2,1};
+        Tableaux.trierDecroissantSSS(tab);
+        assertArrayEquals(expectedTab, tab);
+    }
+
+    @org.junit.Test
+    public void testTrierDecroissantSSS() {
+    }
+
+    @org.junit.Test
+    public void testTrierCroissantSSS() {
+    }
+
+    @org.junit.Test
+    public void max() {
+        int[] tab = {5,4,2,3,1,6,7,9,8};
+        int expectedResult = 9;
+        int result = Tableaux.max(tab);
+        assertEquals(expectedResult, result);
+
+    }
+
+    @org.junit.Test
+    public void testMax() {
+    }
+
+    @org.junit.Test
+    public void min() {
+        int[] tab = {5,4,2,3,1,6,7,9,8};
+        int expectedResult = 1;
+        int result = Tableaux.min(tab);
+        assertEquals(expectedResult, result);
+    }
+
+    @org.junit.Test
+    public void testMin() {
+    }
+
+    @org.junit.Test
+    public void moyenne() {
+        int[] tab = {5,4,2,3,1,6,7,9,8};
+        int expectedResult = 5;
+        int result = Tableaux.moyenne(tab);
+        assertEquals(expectedResult, result);
+    }
+
+    @org.junit.Test
+    public void testMoyenne() {
+    }
+
+    @org.junit.Test
+    public void fouilleseq() {
+        int[] tab = {5,4,2,3,1,6,7,9,8};
+        int cherche = 5;
+        int expectedResult = 0;
+        int result = Tableaux.fouilleseq(tab,cherche);
+        assertEquals(expectedResult, result);
+    }
+
+    @org.junit.Test
+    public void testFouilleseq() {
+    }
+}
